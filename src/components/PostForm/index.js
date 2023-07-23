79f7803d87e6ea7398948873b582bb7bed181f87
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2023-07-23 16:29:50 +0630

    select-form-useState

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 export default function Index({addPost}) {
 
     let [title, setTitle ] = useState("");
+    let [status, setStatus] = useState("upcoming");
 
     let resetForm = () => {
         setTitle('');
@@ -15,7 +16,8 @@ export default function Index({addPost}) {
         e.preventDefault();
         let post = {
             id : Math.floor(Math.random()*10000),
-            title : title
+            title : title,
+            status : status
         }
         resetForm();
         addPost(post);
@@ -27,10 +29,18 @@ export default function Index({addPost}) {
 
   return (
     <form className='post-form' onSubmit={upload_post}>
-        <h1>Create Post</h1>
+        <h1>Create Post {status}</h1>
         <div className='form-control'>
             <label>Title</label>
             <input type='text'onChange={(e) => setTitle(e.target.value)} value={title}/>
+        </div>
+        <div className='form-control'>
+            <label>Status</label>
+            <select value={status} onChange={(e) => setStatus(e.target.value)}>
+                <option value="dropped">Dropped</option>
+                <option value="ongoing">Ongoing</option>
+                <option value="upcoming">Upcoming</option>
+            </select>
         </div>
         <div className='form-control'>
             <button type='submit'>Post Now</button>
